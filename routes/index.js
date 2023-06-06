657ce16922ae11d58a6d5f0e0a952fc946480fe9
Achref LAABIDI <abidi.achref95@gmail.com>
2023-06-06 19:33:30 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,45 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
+const Movies = require('../models/Movie.model');
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-cinema";
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+mongoose
+  .connect(MONGO_URI)
+  .then(()=> 
+    Movies.find({})
+  )
+  .then(item => 
+    res.render('movies', {item})
+    ) 
+  .then(()=> 
+  mongoose.connection.close()
+  )
+  .catch((err) => {
+        console.error("Error connecting to mongo: ", err)
+    })
+})
+
+router.get('/movie/:id', (req, res, next) => {
+    mongoose
+      .connect(MONGO_URI)
+      .then(()=> 
+      Movies.findById(req.params.id)
+      )
+      .then(item => 
+        res.render('moviebyid', item)
+        ) 
+      .then(()=> 
+      mongoose.connection.close()
+      )
+      .catch((err) => {
+        console.error("Error connecting to mongo: ", err)
+    })
+})
 
 module.exports = router;
